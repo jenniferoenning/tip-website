7a8b4ccbff38b6c8f30749c1c054a2309e4c9d2a
jenniferoenning <oenningstangjennifer@gmail.com>
2022-10-26 14:34:39 -0300

    added config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
@@ -12,12 +17,36 @@ module.exports = {
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
+        extend: {},
+        scale: {
+            '0': '0',
+           '25': '.25',
+            '50': '.5',
+            '75': '.75',
+            '90': '.9',
+           '95': '.95',
+            '100': '1',
+           '105': '1.05',
+           '110': '1.1',
+            '125': '1.25',
+            '150': '1.5',
+           '200': '2',
         },
-    },
+        container: {
+          padding: {
+            DEFAULT: '1rem',
+            sm: '2rem',
+            lg: '4rem',
+            xl: '5rem',
+            '2xl': '6rem',
+          },
+        },
+      },
+    variants: {
+        extend: {
+          scale: ['active', 'group-hover'],
+        },
+      },
 
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
