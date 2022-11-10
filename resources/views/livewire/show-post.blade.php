0a20411bea8d5b9335976259025c20996f57ffa6
jenniferoenning <oenningstangjennifer@gmail.com>
2022-11-09 22:06:49 -0300

    adjust show post

--- resources/views/livewire/show-post.blade.php
+++ resources/views/livewire/show-post.blade.php
@@ -1,83 +1,77 @@
-<div>
-    <section>
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Postagem') }}
-            </h2>
-        </x-slot>
+<div class="h-100 w-full justify-center justify-center bg-teal-lightest font-sans pb-5">
+    <div class="container mx-auto bg-white rounded shadow p-6 mt-4 items-center">
+        <section>
+            <x-slot name="header">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Postagem') }}
+                </h2>
+            </x-slot>
 
-        <div class="xl:container mx-auto my-5">
-            <div class="max-w-sm w-full lg:max-w-full lg:flex">
-
-                @if ($post->post_photo_path)
-                    <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('{{ url("storage/{$post->post_photo_path}") }}')" title="{{ $post->user->name }}">
-                    </div>
-                @else
-                    <div class="bg-center h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('{{ url('imgs/no-image-thumb.png') }}')" title="{{ $post->user->name }}">
-                    </div>
-                @endif
-              <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                <div class="mb-8">
-                  <p class="text-sm text-gray-600 flex items-center">
-                    <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                      <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-                    </svg>
-                    Members only
-                  </p>
-                  <div class="text-gray-900 font-bold text-xl mb-2">{{ $post->title }}</div>
-                  <p class="text-gray-700 text-base">{{ $post->description }}</p>
+            <div class="xl:container mx-auto my-5">
+                <h1 class="text-2xl text-gray-600"> {{ $post->title}} </h1>
+                <div class="max-w-sm w-full lg:max-w-full lg:flex justify-center">
+                    @if ($post->post_photo_path)
+                        <div class="flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+                            <img  style="height: 400px;" class="w-full object-cover rounded" src="{{ url("storage/{$post->post_photo_path}") }}" alt="" />
+                        </div>
+                    @else
+                        <div class="bg-center h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+                            <img src="{{ url('imgs/no-image-thumb.png') }}">
+                        </div>
+                    @endif
                 </div>
-                <div class="flex items-center">
-                    <img class="w-10 h-10 rounded-full mr-4" src="{{ url("{$post->user->profile_photo_url}") }}" alt="{{ $post->user->name }}">
-                    <div class="text-sm">
-                        <p class="text-gray-900 leading-none">{{ $post->user->name }}</p>
-                        <p class="text-gray-600">{{ $post->created_at->diffForHumans() }}</p>
-                    </div>
-                </div>
-              </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <section class="col-span-8 col-start-5 mt-10 space-y-6">
-        @auth
-            <form method="POST" action="/posts/{{ $post->slug }}/comentarios" class="border border-gray-200 p-6 rounded-xl">
+        <section class="col-span-8 col-start-5 mt-10 space-y-6">
+            <div class="xl:container mx-auto my-5">
+                @auth
+                <form method="POST" action="/posts/{{ $post->slug }}/comentarios">
                 @csrf
-
-                <header class="flex items-center">
-                    <img class="w-10 h-10 rounded-full mr-4" src="{{ url("{$post->user->profile_photo_url}") }}" alt="{{ $post->user->name }}">
-                    <h2 class="ml-4">Você quer comentar?</h2>
-                </header>
-
-
-                <div class="mt-6">
-                    <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" cols="30" rows="10" placeholder="Escreva aqui seu feedback!"></textarea>
-                </div>
-
-                <div class="flex justify-end mt-10">
-                    <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Postar comentário</button>
-                </div>
-            </form>
-        @else
-            <p class="font-semibold">
-                <a href="/register" class="hover:underline">Registre para comentar!</a> ou
-                <a href="/login" class="hover:underline">Log in</a> para comentar.
-            </p>
-        @endauth
-        
-    </section>
-
-
-    <section class="col-span-8 col-start-5 mt-10 space-y-6">
-        <article class="flex bg-gray-200 border border-gray-300 p-6 rounded-xl rounded space-x-4">
-            <div class="flex-shrink-0">
-                <img src="{{ url("{$post->user->profile_photo_url}") }}" alt="{{ $post->user->name }}" width="60" height="60" class="rounded">
+                    <div class="mb-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+                       <div class="py-2 px-4 bg-white rounded-b-lg dark:bg-gray-800">
+                           <textarea name="body" rows="8" class="block px-0 w-full text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Escreva um comentário..."></textarea>
+                       </div>
+                    </div>
+                    <div class="flex">
+                        <div>
+                            <img class="w-10 h-10 mr-4 rounded-full object-cover" src="{{ url("{$post->user->profile_photo_url}") }}" alt="{{ $post->user->name }}">
+                        </div>
+                        <div class="flex">
+                            <button type="submit" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-orange-300 rounded-lg focus:ring-4 focus:ring-orange-400 hover:bg-orange-400">
+                            Públicar comentário
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                @else
+                    <p class="font-semibold">
+                        <a href="/register" class="hover:underline">Registre para comentar!</a> ou
+                        <a href="/login" class="hover:underline">Log in</a> para comentar.
+                    </p>
+                @endauth
             </div>
-            <header>
-                <h3 class="front-bold">{{ $post->user->name }}</h3>
-                <p class="text-xs">Postado <time>8 meses atrás</time></p>
-            </header>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-        </article>
-    </section>
+            
+        </section>
+
+        <section class="col-span-8 col-start-5 mt-10 space-y-6">
+            <div class="xl:container mx-auto my-5">
+                <article class="bg-gray-50 border border-gray-100 p-6 rounded-xl rounded">
+                    <div class="flex space-x-4">
+                        <div class="flex-shrink-0">
+                            <img src="{{ url("{$post->user->profile_photo_url}") }}" alt="{{ $post->user->name }}" width="60" height="60" class="rounded">
+                        </div>
+                        <header>
+                            <h3 class="front-bold">{{ $post->user->name }}</h3>
+                            <p class="text-xs text-gray-600">Postado <time>8 meses atrás</time></p>
+                        </header>
+                    </div>
+                    <br>
+                    <div>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    </div>
 </div>
