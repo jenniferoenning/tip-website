0f606ef6ca1eb0585d313f4e3f76457837645fbb
jenniferoenning <oenningstangjennifer@gmail.com>
2022-11-16 15:17:26 -0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Model::unguard();
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
