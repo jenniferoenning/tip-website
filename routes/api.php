a2f62f4f5d788e1ffc3a94d51b55e35182e63ed0
jenniferoenning <oenningstangjennifer@gmail.com>
2022-12-01 18:23:13 -0300

    test

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\testeController;
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,6 +24,8 @@ Route::put('comment/{id}/edit', [CommentController::class, 'edit']);
 Route::apiResource('posts', PostController::class);
 Route::get('post/{id}', [PostController::class, 'show']);
 Route::put('post/{id}/edit', [PostController::class, 'edit']);
+
+Route::apiResource('/getSentiment', testeController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
