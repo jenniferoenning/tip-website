3651889b9a632663e1fac1ad155e7c73ae0bce41
jenniferoenning <oenningstangjennifer@gmail.com>
2022-12-05 20:13:45 -0300

    change text

--- resources/views/livewire/show-post.blade.php
+++ resources/views/livewire/show-post.blade.php
@@ -95,7 +95,7 @@
                         </div>
                         <div class="flex">
                             <button type="submit" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-orange-300 rounded-lg focus:ring-4 focus:ring-orange-400 hover:bg-orange-400">
-                            Públicar comentário
+                            Publicar comentário
                             </button>
                         </div>
                     </div>
